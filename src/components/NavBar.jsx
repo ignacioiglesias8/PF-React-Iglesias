70395aa7b5e00c8a69c52748f2a29a933fa6cdfb
Ignacio Iglesias <ignacioiglesias8@gmail.com>
2023-05-21 13:56:13 -0300

    creando repo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -13,14 +13,14 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-            <Nav.Link id="navLink" href="#home">Home</Nav.Link>
-            <Nav.Link id="navLink" href="#link">Excursiones</Nav.Link>
-            <NavDropdown title="Categorias" id="navLink" className="basic-nav-dropdown navLink">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Excursiones</Nav.Link>
+            <NavDropdown title="Categorias" className="basic-nav-dropdown">
                 <NavDropdown.Item className="navItem" href="#action/3.1">Valle</NavDropdown.Item>
                 <NavDropdown.Item className="navItem" href="#action/3.2">Quebrada</NavDropdown.Item>
                 <NavDropdown.Item className="navItem" href="#action/3.3">Yungas</NavDropdown.Item>
                 <NavDropdown.Item className="navItem" href="#action/3.4">Puna</NavDropdown.Item>
-                <NavDropdown title="Exterior" id="navLink" className="basic-nav-dropdown">
+                <NavDropdown title="Exterior" className="basic-nav-dropdown">
                     <NavDropdown.Item className="navItem" href="#action/2.1">Tarija</NavDropdown.Item>
                     <NavDropdown.Item className="navItem" href="#action/2.2">Atacama</NavDropdown.Item>
                     </NavDropdown>
