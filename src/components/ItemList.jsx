b64cbd5179af966fcf98433d7f4446fcf9bc0d95
Ignacio Iglesias <ignacioiglesias8@gmail.com>
2023-05-28 02:36:52 -0300

    agregando más objetos al array

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -3,7 +3,7 @@ import Item from "./Item";
 const ItemList = ({tours}) => {
     return (
         <div className='listGroup'>
-            {tours.map(tour => <Item key={tour.id} {...tour} />)}
+            {tours.map(tour => <Item key={tour.id} {...tour} />)}            
         </div>
     )
 }
--- src/listadoTours.js
+++ src/listadoTours.js
@@ -4,6 +4,18 @@ import yala from "./assets/img/yala.jpg";
 import reyes from "./assets/img/reyes.jpg";
 import tiraxi from "./assets/img/tiraxi.jpg";
 import purmamarca from "./assets/img/purmamarca.jpg";
+import maimara from "./assets/img/maimara.jpg";
+import tilcara from "./assets/img/tilcara.jpg";
+import uquia from "./assets/img/uquia.jpg";
+import humahuaca from "./assets/img/humahuaca.jpg";
+import trescruces from "./assets/img/trescruces.jpg";
+import abrapampa from "./assets/img/abrapampa.jpg";
+import casabindo from "./assets/img/casabindo.jpg";
+import cochinoca from "./assets/img/cochinoca.jpg";
+import rinconada from "./assets/img/rinconada.jpg";
+import cusicusi from "./assets/img/cusicusi.jpg";
+import abdon from "./assets/img/abdon.jpg";
+import yavi from "./assets/img/yavi.jpg";
 
 const tours = [
     {
@@ -21,7 +33,7 @@ const tours = [
         nombre: 'El Carmen',
         categoria: 'Jujuy',
         subcategoria: 'Valle',
-        precio: 1200,
+        precio: 1300,
         stock: 10,
         imagen: elCarmen,
         descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
@@ -31,7 +43,7 @@ const tours = [
         nombre: 'Yala',
         categoria: 'Jujuy',
         subcategoria: 'Valle',
-        precio: 1300,
+        precio: 1500,
         stock: 10,
         imagen: yala,
         descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
@@ -41,7 +53,7 @@ const tours = [
         nombre: 'Reyes',
         categoria: 'Jujuy',
         subcategoria: 'Valle',
-        precio: 1500,
+        precio: 1700,
         stock: 10,
         imagen: reyes,
         descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
@@ -51,7 +63,7 @@ const tours = [
         nombre: 'Tiraxi',
         categoria: 'Jujuy',
         subcategoria: 'Valle',
-        precio: 1200,
+        precio: 1500,
         stock: 10,
         imagen: tiraxi,
         descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
@@ -61,9 +73,129 @@ const tours = [
         nombre: 'Purmamarca',
         categoria: 'Jujuy',
         subcategoria: 'Quebrada',
-        precio: 1500,
+        precio: 2300,
         stock: 10,
         imagen: purmamarca,
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    },
+    {
+        id: '7',
+        nombre: 'Maimará',
+        categoria: 'Jujuy',
+        subcategoria: 'Quebrada',
+        precio: 2300,
+        stock: 10,
+        imagen: maimara,
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    },
+    {
+        id: '8',
+        nombre: 'Tilcara',
+        categoria: 'Jujuy',
+        subcategoria: 'Quebrada',
+        precio: 2500,
+        stock: 10,
+        imagen: tilcara,
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    },
+    {
+        id: '9',
+        nombre: 'Uquía',
+        categoria: 'Jujuy',
+        subcategoria: 'Quebrada',
+        precio: 2500,
+        stock: 10,
+        imagen: uquia,
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    },
+    {
+        id: '10',
+        nombre: 'Humahuaca',
+        categoria: 'Jujuy',
+        subcategoria: 'Quebrada',
+        precio: 2800,
+        stock: 10,
+        imagen: humahuaca,
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    },
+    {
+        id: '11',
+        nombre: 'Tres Cruces',
+        categoria: 'Jujuy',
+        subcategoria: 'Quebrada',
+        precio: 2500,
+        stock: 10,
+        imagen: trescruces,
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    },
+    {
+        id: '12',
+        nombre: 'Abra Pampa',
+        categoria: 'Jujuy',
+        subcategoria: 'Puna',
+        precio: 2500,
+        stock: 10,
+        imagen: abrapampa,
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    },
+    {
+        id: '13',
+        nombre: 'Casabindo',
+        categoria: 'Jujuy',
+        subcategoria: 'Puna',
+        precio: 2800,
+        stock: 10,
+        imagen: casabindo,
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    },
+    {
+        id: '14',
+        nombre: 'Cochinoca',
+        categoria: 'Jujuy',
+        subcategoria: 'Puna',
+        precio: 2800,
+        stock: 10,
+        imagen: cochinoca,
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    },
+    {
+        id: '15',
+        nombre: 'Rinconada',
+        categoria: 'Jujuy',
+        subcategoria: 'Puna',
+        precio: 3000,
+        stock: 10,
+        imagen: rinconada,
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    },
+    {
+        id: '16',
+        nombre: 'Abdon Castro Tolay',
+        categoria: 'Jujuy',
+        subcategoria: 'Puna',
+        precio: 3000,
+        stock: 10,
+        imagen: abdon,
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    },
+    {
+        id: '17',
+        nombre: 'Cusi Cusi',
+        categoria: 'Jujuy',
+        subcategoria: 'Puna',
+        precio: 3500,
+        stock: 10,
+        imagen: cusicusi,
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    },
+    {
+        id: '18',
+        nombre: 'Yavi',
+        categoria: 'Jujuy',
+        subcategoria: 'Puna',
+        precio: 3500,
+        stock: 10,
+        imagen: yavi,
         descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
     },
 ]
