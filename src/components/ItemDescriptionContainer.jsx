d7428a4cad1a21d64cd8dbce35f64817ebd83e9a
Ignacio Iglesias <ignacioiglesias8@gmail.com>
2023-06-22 13:23:32 -0300

    subiendo clase 13

--- src/components/ItemDescriptionContainer.jsx
+++ src/components/ItemDescriptionContainer.jsx
@@ -1,26 +1,39 @@
 import React, {useState, useEffect} from "react";
-import {getTourById} from "../listadoTours";
 import Description from "./Description";
-import {useParams} from 'react-router-dom'
+import {useParams} from 'react-router-dom';
+import {doc, getDoc, getFirestore} from "firebase/firestore";
+import Loader from "./Loader";
 
 const ItemDescriptionContainer = () => {
     const[pack, setPack] = useState(null)
-
+    const [loading, setLoading] = useState(true)
     const {itemId} = useParams();
 
     useEffect(() => {
-        getTourById(itemId) 
-        .then(response =>{
-            setPack(response)
-        })
-        .catch(error => {
-            console.log(error)
-        })
-    }, [itemId])
+        const db = getFirestore()
+        const docRef =doc(db,'tours', itemId)
+
+        getDoc(docRef)
+            .then(response=>{
+                const data =response.data()
+                const productsAdapted = {id:response.id, ...data}
+                setPack(productsAdapted)
+            })
+            .catch(error =>{
+                console.log(error)
+            })
+            .then(()=>{
+                setLoading(false)
+            })
+    },)
 
     return (
         <div className="itemDescriptionContainer">
+                    {loading ? (
+            <Loader/>
+        ):(
             <Description {...pack}/>
+        )}
         </div>
     );
 };
