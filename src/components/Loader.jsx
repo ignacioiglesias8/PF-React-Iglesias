8ae20341becf68c6669dd204aab556bb22132a03
Ignacio Iglesias <ignacioiglesias8@gmail.com>
2023-06-22 01:58:29 -0300

    subiendo clase 13

---
+++ src/components/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Ring } from '@uiball/loaders';
+
+export default function Loader() {
+    return (
+        <Ring 
+            size={40}
+            lineWeight={5}
+            speed={2} 
+            color="black"
+        />
+    )
+}
